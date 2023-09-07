c6770326b8f4a962418364bc035b8f39b13a03fa
Billal Hossain <billal4b@gmail.com>
2023-09-07 16:50:40 +0600

    Quiz App First comment

---
+++ app/src/main/java/com/example/quizapplication/QuestionAnswer.java
@@ -0,0 +1,25 @@
+package com.example.quizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "1. Which company owns the android?",
+            "2. Which one is not the programming language?",
+            "3. Where you are watching this video?",
+            "4. Which company owns the Apple?"
+    };
+
+    public static String choices[][] = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Facebook","Whatsapp","Instagram","Youtube"},
+            {"Google","Apple","Nokia","Samsung"}
+    };
+
+    public static String correctAnswers[] = {
+            "Google",
+            "Notepad",
+            "Youtube",
+            "Apple"
+    };
+}
